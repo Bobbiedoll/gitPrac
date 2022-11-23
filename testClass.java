bc0e60c84eaac45c56b758743f3a4f76fbb0b3b6
Bobbiedoll <Bobbiejeancash88@gmail.com>
2022-11-23 10:30:12 -0600

    Create testClass.java

---
+++ testClass.java
@@ -0,0 +1,17 @@
+package gitPrac;
+
+public class GitHubPrac implements IDK {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello GitHub please work");
+		
+	}
+
+	@Override
+	public  void poop() {
+		// TODO Auto-generated method stub
+		System.out.println("poop");
+	}
+
+}
